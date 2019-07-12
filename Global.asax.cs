bc08f23eed5e1b4513f352834d59cce02797b134
Yordan Ivanov <y.ivanov@synergysportstech.com>
2019-07-12 07:02:44 +0300

    Add project files.

---
+++ Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using CarShow.DataAaccessLayer.Helpers;
+using System.Data.Entity;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace CarShow
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the carshow database.
+            Database.SetInitializer(new CarShowInitializer());
+        }
+    }
+}
